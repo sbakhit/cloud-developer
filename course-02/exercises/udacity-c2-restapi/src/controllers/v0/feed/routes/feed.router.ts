7bd2c5234949b30e8b26ede7b0fb25f0d098c2ef
sbakhit <saleh.bakhit@hotmail.com>
2020-02-04 20:32:18 -0500

    add: feed/get & feed/patch

--- course-02/exercises/udacity-c2-restapi/src/controllers/v0/feed/routes/feed.router.ts
+++ course-02/exercises/udacity-c2-restapi/src/controllers/v0/feed/routes/feed.router.ts
@@ -18,53 +18,74 @@ router.get('/', async (req: Request, res: Response) => {
 
 //@TODO
 //Add an endpoint to GET a specific resource by Primary Key
+router.get('/:id', async (req: Request, res: Response) => {
+    let { id } = req.params;
+    const item = await FeedItem.findByPk(id);
+    if( !item ) {
+        res.status(404).send(`item with id ${id} not found`);
+    }
+
+    res.status(200).send(item);
+});
 
 // update a specific resource
 router.patch('/:id',
-    requireAuth,
-    async (req: Request, res: Response) => {
+    requireAuth, async (req: Request, res: Response) => {
         //@TODO try it yourself
-        res.send(500).send("not implemented")
-});
+        let { id } = req.params;
+        const caption = req.body.caption;
+        const fileName = req.body.url
 
+        const item = await FeedItem.findByPk(id);
+        if( !item ) {
+            res.status(404).send(`item with id ${id} not found`);
+        }
+
+        if( caption ) {
+            item.caption = caption;
+        }
+        if( fileName ) {
+            item.url = fileName;
+        }
+        const updatedItem = await item.save();
+        res.status(201).send(updatedItem);
+});
 
 // Get a signed url to put a new item in the bucket
 router.get('/signed-url/:fileName',
-    requireAuth,
-    async (req: Request, res: Response) => {
-    let { fileName } = req.params;
-    const url = AWS.getPutSignedUrl(fileName);
-    res.status(201).send({url: url});
+    requireAuth, async (req: Request, res: Response) => {
+        let { fileName } = req.params;
+        const url = AWS.getPutSignedUrl(fileName);
+        res.status(201).send({url: url});
 });
 
 // Post meta data and the filename after a file is uploaded
 // NOTE the file name is they key name in the s3 bucket.
 // body : {caption: string, fileName: string};
 router.post('/',
-    requireAuth,
-    async (req: Request, res: Response) => {
-    const caption = req.body.caption;
-    const fileName = req.body.url;
+    requireAuth, async (req: Request, res: Response) => {
+        const caption = req.body.caption;
+        const fileName = req.body.url;
 
-    // check Caption is valid
-    if (!caption) {
-        return res.status(400).send({ message: 'Caption is required or malformed' });
-    }
+        // check Caption is valid
+        if (!caption) {
+            return res.status(400).send({ message: 'Caption is required or malformed' });
+        }
 
-    // check Filename is valid
-    if (!fileName) {
-        return res.status(400).send({ message: 'File url is required' });
-    }
+        // check Filename is valid
+        if (!fileName) {
+            return res.status(400).send({ message: 'File url is required' });
+        }
 
-    const item = await new FeedItem({
+        const item = await new FeedItem({
             caption: caption,
             url: fileName
-    });
+        });
 
-    const saved_item = await item.save();
+        const saved_item = await item.save();
 
-    saved_item.url = AWS.getGetSignedUrl(saved_item.url);
-    res.status(201).send(saved_item);
+        saved_item.url = AWS.getGetSignedUrl(saved_item.url);
+        res.status(201).send(saved_item);
 });
 
 export const FeedRouter: Router = router;
